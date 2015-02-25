968477722d659d3d9473c3fb51f0ba6870997693
Angel S Chavez <salomon7983@gmail.com>
2015-02-24 17:07:54 -0700

    My First Commit

---
+++ app/models/listing.rb
@@ -0,0 +1,19 @@
+class Listing < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "250x>" , :thumb => "150x150" }  , :default_url => "missing.png"
+validates_attachment :image, :content_type => { :content_type => ["image/jpg", "image/gif", "image/png"] }
+end
+
+#["image/jpg", "image/gif", "image/png"]
+# has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.png"
+# I tried this but hot an error
+#validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
+#
+#
+ #do_not_validate_attachment_file_type :image
+
+ #this is the working version
+ # 
+  #validates_attachment_content_type :image, :content_type => { :content_type => %w(image/jpeg image/jpg image/png) }
+
+  #other version
+#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,35 @@
+module Paperclip
+class MediaTypeSpoofDetector
+def spoofed?
+false
+end
+end
+end
+
+
+
+
+
+
+
+
+
+#module Paperclip
+ # class MediaTypeSpoofDetector
+  #  def type_from_file_command
+   #   begin
+    #    Paperclip.run("file", "-b --mime :file", :file => @file.path)
+     # rescue Cocaine::CommandLineError
+      #  ""
+      #end
+    #end
+  #end
+#end
+
+
+
+
+
+
+
+
